ccdd8be65a03c632a1677a632d417fa78327b64e
Omri A <devomri@gmail.com>
2017-02-18 23:27:50 +0200

    changed main

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ def main():
     print("Songs Analyzer Project")
     songs_analyzer = analyzer.SongsAnalyzer()
 
-    """
-    rappers_list = ["kool_keith", "canibus", "cunninlynguists", "rza", "wu-tang", "killah_priest", "Drake"]
+    rappers_list = ["kool_keith", "canibus", "cunninlynguists", "rza", "wu-tang", "killah_priest", "eminem"]
+    rappers_diversity_sum = 0
     for artist in rappers_list:
         diversity = songs_analyzer.lexical_diversity_by_artist(artist)
+        rappers_diversity_sum += diversity
         print("Rapper: {0} has lexical diversity of {1}".
               format(artist, diversity))
-    """
+    rappers_diversity_avg = rappers_diversity_sum / len(rappers_list)
+    print("Average diversity for rappers: {0}\n".format(rappers_diversity_avg))
 
     pop_artists = ["lady_gaga", "big_sean", "ed_sheeran", "bruno_mars", "drake",
                    "migos", "rihanna", "adele", "twenty_one_pilots",
                    "ariana_grande", "taylor_swift", "maroon_5", "justin_bieber",
-                   "imagine_dragons", "john_legend", "metallica", "sia", "train", "eminem"]
+                   "imagine_dragons", "john_legend", "metallica", "sia", "train"]
+    pop_diversity_sum = 0
     for artist in pop_artists:
         diversity = songs_analyzer.lexical_diversity_by_artist(artist)
+        pop_diversity_sum += diversity
         print("Artist: {0} has lexical diversity of {1}".
               format(artist, diversity))
+    pop_diversity_avg = pop_diversity_sum / len(pop_artists)
+    print("Average diversity for pop artists is: {0}".format(pop_diversity_avg))
 
     """
     years_intervals = [(1970, 1979), (1980, 1989), (1990, 1999), (2000, 2009)]
