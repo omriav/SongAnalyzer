d0ae919ee1208bc79af194c8c73c4f346649f132
Omri A <devomri@gmail.com>
2017-02-18 19:04:10 +0200

    added elasticsearch suuport

--- DataLayer/SongsFactory.py
+++ DataLayer/SongsFactory.py
@@ -1,10 +1,30 @@
 import json
 import gzip
+from elasticsearch import Elasticsearch
 
 
 class SongFactory:
+    ES_INDEX = "allsonglyrics"
+    ES_TYPE = "song"
 
+    def __init__(self):
+        self.es = Elasticsearch()
 
+    def get_songs_by_artist(self, artist_name):
+        q = {
+            "query": {
+                "match": {
+                    "_body.artist.unique_name": "Red_Hot_Chili_Peppers"
+                }
+            }
+        }
+
+        res = self.es.search(index=self.ES_INDEX, doc_type=self.ES_TYPE, body=q)
+        parsed_result = [h["_source"]["_body"] for h in res["hits"]["hits"]]
+
+        return parsed_result
+
+"""
     def get_songs_by_artist(self, artist_name):
         DATA_URL = "/home/omri/Dev/Python/IntroToDS/Data/parsed_lyrics/{0}"
         songs_list = []
@@ -13,3 +33,4 @@ class SongFactory:
             songs_list = json.loads(gzip.decompress(data_file.read()).decode())
 
         return songs_list
+"""
--- ProcessingLayer/SongsAnalyzer.py
+++ ProcessingLayer/SongsAnalyzer.py
@@ -1,3 +1,4 @@
+import nltk
 import DataLayer.SongsFactory as factory
 import re
 
@@ -11,8 +12,13 @@ class SongsAnalyzer:
     def get_words_list_by_artist(self, artist_name):
         words = []
         for song in self.songs_factory.get_songs_by_artist(artist_name):
-            curr_lyrics = re.sub("[^A-Za-z0-9[.]]", "", song["lyrics"])
-            curr_lyrics = curr_lyrics.lower()
-            words += curr_lyrics.split()
+            #curr_lyrics = re.sub("[^A-Za-z0-9[.]]", "", song["lyrics"])
+            #curr_lyrics = curr_lyrics.lower()
+            curr_lyrics = nltk.word_tokenize(song["lyrics"])
+            words += curr_lyrics
+
+            if (len(words) >= self.WORDS_THRESHOLD):
+                words = words[:self.WORDS_THRESHOLD]
+                break
 
         return words
