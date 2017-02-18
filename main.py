7317e8bbb0453cb5b3578d09ca8094f49d47ee54
Omri A <devomri@gmail.com>
2017-02-18 21:31:35 +0200

    rappers temp list

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import ProcessingLayer.SongsAnalyzer as analyzer
 
 def main():
     print("Songs Analyzer")
+    rappers_list = ["kool_keith", "canibus", "cunninlynguists", "rza", "wu-tang", "killah_priest", "Drake"]
+
     songs_analyzer = analyzer.SongsAnalyzer()
-    songs_analyzer.lexical_diversity_by_artist("Kool_keith")
+
+    for artist in rappers_list:
+        diversity = songs_analyzer.lexical_diversity_by_artist(artist)
+        print("Rapper: {0} has lexical diversity of {1}".
+              format(artist, diversity))
 
 
 if __name__ == '__main__':
